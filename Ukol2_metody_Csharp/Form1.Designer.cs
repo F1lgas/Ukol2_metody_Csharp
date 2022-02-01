3e14beb719fd29c62fdf7d3026b35ba10afdec7d
Filip Iran <iranf.03@spst.cz>
2022-02-01 16:45:54 +0100

    v1.0

--- Ukol2_metody_Csharp/Form1.Designer.cs
+++ Ukol2_metody_Csharp/Form1.Designer.cs
@@ -28,13 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cisla = new System.Windows.Forms.TextBox();
+            this.generovat = new System.Windows.Forms.Button();
+            this.prvocisla = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.vstup_N = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cisla
+            // 
+            this.cisla.Location = new System.Drawing.Point(12, 54);
+            this.cisla.Multiline = true;
+            this.cisla.Name = "cisla";
+            this.cisla.ReadOnly = true;
+            this.cisla.Size = new System.Drawing.Size(113, 95);
+            this.cisla.TabIndex = 0;
+            // 
+            // generovat
+            // 
+            this.generovat.Location = new System.Drawing.Point(172, 10);
+            this.generovat.Name = "generovat";
+            this.generovat.Size = new System.Drawing.Size(75, 23);
+            this.generovat.TabIndex = 1;
+            this.generovat.Text = "Generovat";
+            this.generovat.UseVisualStyleBackColor = true;
+            this.generovat.Click += new System.EventHandler(this.generovat_Click);
+            // 
+            // prvocisla
+            // 
+            this.prvocisla.FormattingEnabled = true;
+            this.prvocisla.Location = new System.Drawing.Point(131, 54);
+            this.prvocisla.Name = "prvocisla";
+            this.prvocisla.Size = new System.Drawing.Size(116, 95);
+            this.prvocisla.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vygenerovaná čísla";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prvočísla";
+            // 
+            // vstup_N
+            // 
+            this.vstup_N.Location = new System.Drawing.Point(66, 12);
+            this.vstup_N.Name = "vstup_N";
+            this.vstup_N.Size = new System.Drawing.Size(100, 20);
+            this.vstup_N.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Zadej N:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(262, 163);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.vstup_N);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.prvocisla);
+            this.Controls.Add(this.generovat);
+            this.Controls.Add(this.cisla);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Úkol 2 metody C#";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox cisla;
+        private System.Windows.Forms.Button generovat;
+        private System.Windows.Forms.ListBox prvocisla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox vstup_N;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Ukol2_metody_Csharp/Form1.cs
+++ Ukol2_metody_Csharp/Form1.cs
@@ -16,5 +16,60 @@ namespace Ukol2_metody_Csharp
         {
             InitializeComponent();
         }
+
+        private bool JePrvocislo(int cislo, bool pravda)
+        {
+            double cislo2 = cislo / 2;
+            bool kontrola = true;
+
+            for (int i = 2; i <= cislo2; i++)
+            {
+                if (cislo % i == 0)
+                {
+                    pravda = false;
+                    kontrola = false;
+                }
+            }
+
+            if (kontrola == true)
+            {
+                pravda = true;
+            }
+
+            return pravda;
+        }
+
+        private void Prepis(int N)
+        {
+            Random rnd = new Random();
+            int[] pole = new int[N];
+
+            for (int i = 0; i < pole.Length; i++)
+            {
+                pole[i] = rnd.Next(2, 16);
+
+                cisla.AppendText(Convert.ToString(pole[i]));
+                cisla.AppendText(Environment.NewLine);
+
+                bool pravda = false;
+                pravda = JePrvocislo(pole[i], pravda);
+
+                if (pravda == true)
+                {
+                    prvocisla.Items.Add(pole[i]);
+                    pravda = false;
+                }
+            }
+        }
+
+        private void generovat_Click(object sender, EventArgs e)
+        {
+            cisla.Clear();
+            prvocisla.Items.Clear();
+
+            int N = Convert.ToInt32(vstup_N.Text);
+            
+            Prepis(N);
+        }
     }
 }
